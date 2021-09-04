4ce6ebd1663cae5d87ca1f71d6534e85d28fb7fc
Hackr <81916900+123452123-codr@users.noreply.github.com>
2021-09-04 13:56:28 +0530

    Update and rename python code to code.py

--- code.py
+++ code.py
@@ -3,10 +3,7 @@ import random
 
 # Print multiline instruction
 # performstring concatenation of string
-print("Winning Rules of the Rock paper scissor game as follows: \n"
-								+"Rock vs paper->paper wins \n"
-								+ "Rock vs scissor->Rock wins \n"
-								+"paper vs scissor->scissor wins \n")
+print("Winning Rules of the Rock paper scissor game as follows: \n"+"Rock vs paper->paper wins \n"+ "Rock vs scissor->Rock wins \n"+"paper vs scissor->scissor wins \n")
 
 while True:
 	print("Enter choice \n 1. Rock \n 2. paper \n 3. scissor \n")
